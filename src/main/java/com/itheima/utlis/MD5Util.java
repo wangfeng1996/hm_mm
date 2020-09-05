4da52622f1742b585a1edfc9533b5bdb0be60616
wangfeng <1290715192@qq.com>
2020-09-05 19:41:51 +0800

    工具类

---
+++ src/main/java/com/itheima/utlis/MD5Util.java
@@ -0,0 +1,28 @@
+package com.itheima.utlis;
+
+import java.security.MessageDigest;
+import sun.misc.BASE64Encoder;
+
+public class MD5Util {
+
+    /**
+     * 密码加密
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    public static String  md5(String password){
+        try {
+            //1.创建加密对象
+            MessageDigest md5 = MessageDigest.getInstance("md5");
+            //2.加密密码
+            byte[] by = md5.digest(password.getBytes());
+            //3.创建编码对象
+            BASE64Encoder encoder = new BASE64Encoder();
+            //4.对结果编码
+            return encoder.encode(by);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
